44d68f722d868692ce75cc4a0beea5f7cdefd847
Yuzukiko <12223625+Yuzukiko@users.noreply.github.com>
2022-06-11 13:52:18 +0200

    Add files via upload

---
+++ Chocolatey_install_all.cmd
@@ -0,0 +1,52 @@
+#PowerShell install script for windows apps
+#requiers Chocolatey installed
+#run as admin
+
+#apps
+choco install thunderbird
+choco install firefox
+choco install googlechrome
+choco install googledrive
+choco install vscode
+choco install onenote
+choco install discord
+choco install virtualbox
+choco install openvpn
+choco install drawio
+choco install microsoft-teams
+choco install spotify
+choco install 7zip
+choco install github-desktop
+choco install javaruntime
+choco install filezilla
+choco install mobaxterm
+choco install postman
+choco install python3
+choco install rpi-imager
+choco install qbittorrent
+choco install homeassistant
+choco install anydesk
+choco install hamachi
+choco install msiafterburner
+choco install mysql.workbench
+choco install obs-studio
+choco install autohotkey
+choco install telegram
+choco install 1password
+
+#docker
+choco install docker-desktop
+choco install docker-compose
+choco install kubernetes-kompose
+
+
+#games
+choco install epicgameslauncher
+choco install steam
+choco install steamcmd
+choco install origin
+choco install multimc
+
+
+
+
